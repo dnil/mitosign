aad5d766e1ef94930f060abe8e619f9f52f8fb09
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2020-02-18 16:35:53 +0100

    Add simple mt del check script.

---
+++ run_mt_del_check.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+#SBATCH -n 1
+#SBATCH -t 00:10:00
+#SBATCH -J run_mitodel
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=daniel.nilsson@scilifelab.se
+#SBATCH -A local
+#SBATCH --qos=normal
+
+BAM_FILE=$1
+OUT_DIR=$2
+
+BAM_FILE_BASE=`basename ${BAM_FILE}`
+SAMPLE=${BAM_FILE_BASE%%_*.bam}
+
+samtools stats -i 16000 $BAM_FILE MT > ${OUT_DIR}/${SAMPLE}.mt_bam_stat
+
+grep ^IS ${OUT_DIR}/${SAMPLE}.mt_bam_stat |awk '($2>=3000 && $2<=15000) {sum=sum+$3} ($2<3000 || $2>15000) {sum_norm=sum_norm+$3} END {print "intermediate discordant ", sum, "normal ", sum_norm, "ratio ppk", sum*1000/(sum_norm+sum)}' > ${OUT_DIR}/${SAMPLE}.mt_del_summary
+
