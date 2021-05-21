59a50c382640646fd50d9b35a064256f39ef3f3c
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2021-05-21 06:33:06 +0200

    lower insert size limit

--- run_mt_del_check.sh
+++ run_mt_del_check.sh
@@ -1,10 +1,9 @@
 #!/bin/bash -l
-#SBATCH -n 1
+#SBATCH -c 1
 #SBATCH -t 00:10:00
 #SBATCH -J run_mitodel
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=daniel.nilsson@scilifelab.se
-#SBATCH -A local
 #SBATCH --qos=normal
 
 BAM_FILE=$1
@@ -13,7 +12,8 @@ OUT_DIR=$2
 BAM_FILE_BASE=`basename ${BAM_FILE}`
 SAMPLE=${BAM_FILE_BASE%%_*.bam}
 
+source activate samtools
 samtools stats -i 16000 $BAM_FILE MT > ${OUT_DIR}/${SAMPLE}.mt_bam_stat
 
-grep ^IS ${OUT_DIR}/${SAMPLE}.mt_bam_stat |awk '($2>=3000 && $2<=15000) {sum=sum+$3} ($2<3000 || $2>15000) {sum_norm=sum_norm+$3} END {print "intermediate discordant ", sum, "normal ", sum_norm, "ratio ppk", sum*1000/(sum_norm+sum)}' > ${OUT_DIR}/${SAMPLE}.mt_del_summary
+grep ^IS ${OUT_DIR}/${SAMPLE}.mt_bam_stat |awk '($2>=1200 && $2<=15000) {sum=sum+$3} ($2<1200 || $2>15000) {sum_norm=sum_norm+$3} END {print "intermediate discordant ", sum, "normal ", sum_norm, "ratio ppk", sum*1000/(sum_norm+sum)}' > ${OUT_DIR}/${SAMPLE}.mt_del_summary
 
